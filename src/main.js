1552efeed1eb8141ac1b3cfb6f9c500c4bbf493f
dlf42139 <dlf42139@163.com>
2019-08-01 09:33:39 +0800

    登录页面功能完成含表单验证

--- src/main.js
+++ src/main.js
@@ -3,11 +3,35 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Vant from 'vant'
+import zh from 'vee-validate/dist/locale/zh_CN'
+import VeeValidate, { Validator } from 'vee-validate'
 import 'vant/lib/index.css'
 import 'amfe-flexible/index.min.js'
 
 Vue.use(Vant)
+Vue.use(VeeValidate)
+Validator.localize('zh_CN', zh)
 Vue.config.productionTip = false
+
+const dictionary = {
+  zh_CN: {
+    messages: {
+      required: (name) => {
+        if (name === 'phone') {
+          return '手机号为必填项'
+        }
+        return name + '为必填项'
+      }
+    }
+  }
+}
+Validator.localize(dictionary)
+
+Validator.extend('phone', {
+  getMessage: field => '请输入正确的手机号码',
+  validate: value =>
+    value.length === 11 && /^((13|14|15|17|18)[0-9]{1}\d{8})$/.test(value)
+})
 
 new Vue({
   router,
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,47 +2,66 @@
   <div>
     <!-- 导航栏 -->
     <van-nav-bar title="欢迎登录" />
-    <!-- 表单域 -->
-    <van-cell-group>
-      <!-- <van-field v-model="phone" label="手机号" placeholder="请输入手机号" error-message="手机号格式错误" /> -->
-      <van-field v-model="user.mobile" label="手机号" placeholder="请输入手机号" />
-      <van-field v-model="user.code" center clearable label="短信验证码" placeholder="请输入短信验证码">
-        <van-button slot="button" size="small" type="primary">发送验证码</van-button>
-      </van-field>
-    </van-cell-group>
-    <!-- 登录按钮 -->
-    <van-button type="info" @click="toLogin">登&nbsp;&nbsp;&nbsp;录</van-button>
+    <form action='/' method="post">
+      <!-- 表单域 -->
+      <van-cell-group>
+        <!-- <van-field v-model="phone" label="手机号" placeholder="请输入手机号" error-message="手机号格式错误" /> -->
+        <van-field
+          v-model="user.mobile"
+          v-validate="'required|phone'"
+          name="phone"
+          label="手机号"
+          placeholder="请输入手机号"
+          :error-message="errors.first('phone')"
+        />
+        <van-field v-model="user.code" center clearable label="短信验证码" placeholder="请输入短信验证码">
+          <van-button slot="button" size="small" type="primary">发送验证码</van-button>
+        </van-field>
+      </van-cell-group>
+      <!-- 登录按钮 -->
+      <!-- 阻止表单的默认跳转 -->
+      <van-button :loading="loginStatus" loading-text="正在登录..." type="info" @click.prevent="toLogin">登&nbsp;&nbsp;&nbsp;录</van-button>
+    </form>
   </div>
 </template>
 
 <script>
 import { login } from '../api/user'
 import { Toast, Notify } from 'vant'
-// import { mapMutations } from 'vuex'
+import { mapMutations } from 'vuex'
 export default {
   data () {
     return {
       user: {
         mobile: '18801185985',
         code: '246810'
-      }
+      },
+      loginStatus: false
     }
   },
   methods: {
-    // ...mapMutations(['setUserToken',data]),
+    ...mapMutations(['setUserToken']), // 这个相当于声明了一个函数
     async toLogin () {
-      try {
-        const data = await login(this.user)
-        console.log(data)
-        this.$store.commit('setUserToken', data)
-        Notify({
-          message: '登录成功',
-          duration: 1000,
-          background: '#1989fa'
-        })
-      } catch (error) {
-        Toast.fail('登录失败')
-      }
+      this.$validator.validate().then(async valid => {
+        if (!valid) {
+          // do stuff if not valid.
+        }
+        this.loginStatus = true
+        try {
+          const data = await login(this.user)
+          console.log(data)
+          // this.$store.commit('setUserToken', data)
+          this.setUserToken(data)
+          Notify({
+            message: '登录成功',
+            duration: 1000,
+            background: '#1989fa'
+          })
+          this.loginStatus = false
+        } catch (error) {
+          Toast.fail('登录失败')
+        }
+      })
     }
   }
 }
